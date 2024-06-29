17e38427e52a967c04dadc36091948311b605066
xingyinggg <xingyinglau.2023@scis.smu.edu.sg>
2024-06-29 15:58:06 +0800

    completed outingsDescription, voteDescription component and reportHeader component. passed on relevant data to placeDetailPage (incomplete btw)

--- src/screens/outings/outingsDescription.js
+++ src/screens/outings/outingsDescription.js
@@ -5,6 +5,7 @@ import {
     StyleSheet,
     SafeAreaView,
     ScrollView,
+    TouchableOpacity,
 } from "react-native";
 import { useRoute } from '@react-navigation/native';
 
@@ -13,10 +14,60 @@ import BackButton from "../../components/backButton";
 import Header1 from "../../components/texts/header1";
 import ReportHeader from "../../components/outings/reportHeader";
 import BottomBar from "../../components/bottomBar";
-import RowDescription from "../../components/rowDescription";
+import VoteDescription from "../../components/outings/voteDescription";
+
+// LINK TO DB FOR VOTE DESCRIPTION
+const outingVotes = [
+    {
+        id: '1',
+        imageSource: require('../../assets/tuftClub.jpg'),
+        name: 'Tuft Club',
+        rating: '5.0',
+        location: 'Circular Road',
+        price: '$$',
+        votes: '6/6',
+    },
+    {
+        id: '2',
+        imageSource: require('../../assets/macRitchieReservoirTreetopLoop.jpg'),
+        name: 'MacRitchie Reservoir',
+        rating: '4.6',
+        location: 'Bukit Pierce',
+        price: 'FOC',
+        votes: '5/6',
+    },
+    {
+        id: '3',
+        imageSource: require('../../assets/isleEatingHouse.jpg'),
+        name: 'Isle Eating House',
+        rating: '4.3',
+        location: '35 Selgie Road',
+        price: '$',
+        votes: '4/6',
+    },
+    {
+        id: '4',
+        imageSource: require('../../assets/macRitchieReservoirTreetopLoop.jpg'),
+        name: 'MacRitchie Reservoir',
+        rating: '4.6',
+        location: 'Bukit Pierce',
+        price: 'FOC',
+        votes: '5/6',
+    },
+    {
+        id: '5',
+        imageSource: require('../../assets/isleEatingHouse.jpg'),
+        name: 'Isle Eating House',
+        rating: '4.3',
+        location: '35 Selgie Road',
+        price: '$',
+        votes: '4/6',
+    },
+    
+    // Add more items here
+];
 
 // Main Component
-//  INCOMPLETE. XY JS WNA SLEEP ALR OK
 const OutingsDescriptionPage = ({ navigation }) => {
     const route = useRoute();
     const { report } = route.params;
@@ -25,17 +76,41 @@ const OutingsDescriptionPage = ({ navigation }) => {
         <SafeAreaView style={styles.container}>
             <ScrollView contentContainerStyle={styles.contentContainer}>
                 <View style={styles.headerRow}>
-                    <BackButton navigation={navigation} />
+                    <View style={styles.backButtonContainer}>
+                        <BackButton navigation={navigation} />
+                    </View>
                     <View style={styles.headerContainer}>
-                        <Header1 text={report.title} />
+                        <ReportHeader reportTitle={report.title} reportDate={report.date} participants={report.participants} />
                     </View>
                 </View>
-                <Text style={styles.date}>{report.date}</Text>
-                <Text style={styles.participants}>{report.participants.join(', ')}</Text>
-                <Header1 text='Activity' />
-                {/* Add your activities and votes here */}
+                <View style={styles.activityHeader}>
+                    <Header1 text='Activity' />
+                    <Header1 text='Votes' style={styles.votesHeader} />
+                </View>
                 <View>
-                    <RowDescription /* Your RowDescription component data here */ />
+                    {outingVotes && outingVotes.map((item) => (
+                        <TouchableOpacity
+                            key={item.id}
+                            onPress={() => navigation.navigate('placeDetailsPage', {
+                                id: item.id,
+                                imageSource: item.imageSource,
+                                name: item.name,
+                                rating: item.rating,
+                                location: item.location,
+                                price: item.price,
+                                votes: item.votes
+                            })}
+                        >
+                            <VoteDescription
+                                imageSource={item.imageSource}
+                                name={item.name}
+                                rating={item.rating}
+                                location={item.location}
+                                price={item.price}
+                                votes={item.votes}
+                            />
+                        </TouchableOpacity>
+                    ))}
                 </View>
             </ScrollView>
             <BottomBar navigation={navigation} />
@@ -60,20 +135,21 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginBottom: 20,
+        justifyContent: 'center',
+    },
+    backButtonContainer: {
+        position: 'absolute',
+        left: 0,
     },
     headerContainer: {
-        marginVertical: 10,
-        marginHorizontal: 10,
         alignItems: 'center',
     },
-    date: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginBottom: 10,
+    activityHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
-    participants: {
-        fontSize: 14,
-        color: '#585858',
-        marginBottom: 20,
+    votesHeader: {
+        textAlign: 'right',
     },
 });
