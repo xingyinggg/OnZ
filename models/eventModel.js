60d892ec5e1284e7e180b28fc8e7ae74390dcb1b
Zephan Chin <zephan.chin.2023@scis.smu.edu.sg>
2024-07-02 18:07:19 +0800

    added event schema

---
+++ models/eventModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const account = new mongoose.Schema({
+    eventID: { type: Number, unique: true },
+    eventName: String,
+    location: String,
+    priceRange: Number,
+    category: String,
+    description: String,
+    photo: String
+})
+
+// Apply the auto-increment pugin to UserID
+account.plugin(AutoIncrement, { inc_field: 'userID' });
+
+
+module.exports = mongoose.model("account", account)
